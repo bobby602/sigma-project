e74b091eae93371aee7d10904d534b77d87807a4
nutdanai.kan <bob6022800@gmail.com>
2022-06-17 11:07:14 +0700

    ss

--- server/api/app.js
+++ server/api/app.js
@@ -2,7 +2,6 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var app = express();
-const line = require('@line/bot-sdk');
 const dotenv = require('dotenv')
 
 var cors = require("cors");
@@ -13,64 +12,13 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'build')));
 
 
-// app.use('/', loginRouter);
-// app.use('/line', lineRouter);
+app.use('/', loginRouter);
+app.use('/line', lineRouter);
 // app.use('/users', usersRouter);
 
 
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'ejs');
-
-
-const config = {
-  channelAccessToken:'4UJs23LOXx3uDm/0mLDdn4AQ3mNKsDOLBp8ETcT1os+n99ILmcTKlAy4fIxcF+9WdmaZuePnGtTszUlWRzmq/2yn9Ot6ffLZ4d7cEqYx09ayNZGYkIe3RjUqVXz6La4Nqge8bLKhY8/OUt/uTNYCqAdB04t89/1O/w1cDnyilFU=',
-  channelSecret: '08441ded22229e8f9c383124d701c7fa'
-}
-
-// create LINE SDK client
-const client = new line.Client(config);
-const router = express.Router();
-
-
-// create Express app
-// about Express itself: https://expressjs.com/
-
-app.get('/', function(req,res){
-      res.json({data:'test'});
-      
-
-});
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
-app.post('/callback', (req, res) => {
-console.log('test')
-Promise
-  .all(req.body.events.map(handleEvent))
-  .then((result) => res.json(result))
-  .catch((err) => {
-    console.error(err);
-    res.status(500).end();
-  });
-});
-
-// event handler
-function handleEvent(event) {
-if (event.type !== 'message' || event.message.type !== 'text') {
-  // ignore non-text-message event
-  return Promise.resolve(null);
-}else if (event.message.type == "text" || event.message.text == "Dow"){
-  const payload = {type:"text" , text :"beautiful"};
-  return client.replyMessage(event.replyToken,payload);
-}
-
-// create a echoing text message
-const echo = { type: 'text', text: event.message.text };
-
-// use reply API
-return client.replyMessage(event.replyToken, echo);
-}
-
-module.exports = router;
 
 app.use(function(req, res, next) {
     next(createError(404));
--- server/api/routes/login.js
+++ server/api/routes/login.js
@@ -12,124 +12,128 @@ const { response } = require('../app');
 const dotenv = require('dotenv')
 const line = require('@line/bot-sdk')
 
-//   var app = express();
-//   const router = express.Router();
-//   router.use(session({
-//       secret: 'secret',
-//       resave:true,
-//       saveUninitialized:true
-//   }));
+  var app = express();
+  const router = express.Router();
+  router.use(session({
+      secret: 'secret',
+      resave:true,
+      saveUninitialized:true
+  }));
  
-//   router.use(express.urlencoded({extended:true}));
-//   router.use(bodyParser.json());
+  router.use(express.urlencoded({extended:true}));
+  router.use(bodyParser.json());
   
-//   router.get('/', function(req,res){
-//     var request = new mssql.Request();
-//     request.query('select * from [DATASIGMA].[dbo].[Users] ',function(err,data,fields){
-//         console.log(data);
-//         res.json({data});
+  router.get('/', function(req,res){
+    var request = new mssql.Request();
+    request.query('select * from [DATASIGMA].[dbo].[Users] ',function(err,data,fields){
+        console.log(data);
+        res.json({data});
         
-//     });
+    });
 
-//   });
+  });
 
-//   router.post('/',function(req,res){
-//     var Login = req.body.username;
-//     console.log(Login);
-//     var password = req.body.password;
-//     console.log(password);
-//     if(Login){
-//         var request = new mssql.Request();
-//         request.input('Login',mssql.VarChar(50),Login);
-//         request.input('Password',mssql.VarChar(50),password);
-//         request.query('select * from [DATASIGMA].[dbo].[Users] where Login = @Login and Password = @Password',function(err,data,fields){
-//             console.log(data);
-//             if(data.rowsAffected > 0){
-//                 console.log('IN');
-//                 req.session.Login = Login;
-//                 res.json({result:"Login Successful"});
-//             }else{
-//                 res.status(400).send({
-//                     result: "There was an issue signing up."
-//                   });
-//             }
-//         });
-//     } else{
-//         res.json('Please Fill Username and Password');
-//     }
-// });
-//   router.get('/table',function(req,res){
-//     const sql = "Select itemdm.codem,itemDm.itemcode,itemdm.Name,itemDm.Barcode, a.p1 as minPrice, a.p2 as maxPrice,TyItemDm,b.QBal,b.BAL " +
-//                 " from DATASIGMA.dbo.ItemDm " + 
-//                 " inner join DATASIGMA.dbo.qitemdmbal " +
-//                 " on itemdm.itemcode=qitemdmbal.itemcode " +
-//                 " inner join ( " +
-//                 " Select min(price) as p1, max(price) as p2 ,ItemCode  from DATASIGMA.dbo.IteminSub group by ItemCode " +
-//                 " ) a " +
-//                 " on a.ItemCode = itemDm.itemcode "+
-//                 "inner join ( " +
-//                     " Select  itemcode,name,sum(qbal +QS2) as QBal,pack, sum(qbal) - sum(QD) - sum(QP1) - sum(qp2) - Sum(QP3) - Sum(QP4)  + Sum(Qs) + Sum(Qs2) as BAL,Note "+
-//                     " From rptstock2  "+
-//                     " Group by itemcode,name,pack ,Note  " +
-//                     " )b on b.itemcode = a.itemcode ; Select *  from DATASIGMA.dbo.QitemBom ";
-//     var db = new mssql.Request();
-//     db.query(sql,function(err,data,fields){
-//         if (err) throw err;
-//             let Data = data.recordset;
-//             let Data2 = data.recordsets[1];
-//             let NewData = new Array(Data.length);
-//             for(let i=0;i<Data.length;i++){
-//                 NewData[i] = Data2.filter((e)=>{
-//                     if(Data[i].itemcode==e.Code){
-//                         return e;
-//                     }
-//                 })
-//                 const NewArr = NewData[i];
-//                 Data[i] = {...Data[i], NewArr};
+  router.post('/',function(req,res){
+    var Login = req.body.username;
+    console.log(Login);
+    var password = req.body.password;
+    console.log(password);
+    if(Login){
+        var request = new mssql.Request();
+        request.input('Login',mssql.VarChar(50),Login);
+        request.input('Password',mssql.VarChar(50),password);
+        request.query('select * from [DATASIGMA].[dbo].[Users] where Login = @Login and Password = @Password',function(err,data,fields){
+            console.log(data);
+            if(data.rowsAffected > 0){
+                console.log('IN');
+                req.session.Login = Login;
+                res.json({result:"Login Successful"});
+            }else{
+                res.status(400).send({
+                    result: "There was an issue signing up."
+                  });
+            }
+        });
+    } else{
+        res.json('Please Fill Username and Password');
+    }
+});
+  router.get('/table',function(req,res){
+    const sql = "Select itemdm.codem,itemDm.itemcode,itemdm.Name,itemDm.Barcode, a.p1 as minPrice, a.p2 as maxPrice,TyItemDm,b.QBal,b.BAL " +
+                " from DATASIGMA.dbo.ItemDm " + 
+                " inner join DATASIGMA.dbo.qitemdmbal " +
+                " on itemdm.itemcode=qitemdmbal.itemcode " +
+                " inner join ( " +
+                " Select min(price) as p1, max(price) as p2 ,ItemCode  from DATASIGMA.dbo.IteminSub group by ItemCode " +
+                " ) a " +
+                " on a.ItemCode = itemDm.itemcode "+
+                "inner join ( " +
+                    " Select  itemcode,name,sum(qbal +QS2) as QBal,pack, sum(qbal) - sum(QD) - sum(QP1) - sum(qp2) - Sum(QP3) - Sum(QP4)  + Sum(Qs) + Sum(Qs2) as BAL,Note "+
+                    " From rptstock2  "+
+                    " Group by itemcode,name,pack ,Note  " +
+                    " )b on b.itemcode = a.itemcode ; Select *  from DATASIGMA.dbo.QitemBom ";
+    var db = new mssql.Request();
+    db.query(sql,function(err,data,fields){
+        if (err) throw err;
+            let Data = data.recordset;
+            let Data2 = data.recordsets[1];
+            let NewData = new Array(Data.length);
+            for(let i=0;i<Data.length;i++){
+                NewData[i] = Data2.filter((e)=>{
+                    if(Data[i].itemcode==e.Code){
+                        return e;
+                    }
+                })
+                const NewArr = NewData[i];
+                Data[i] = {...Data[i], NewArr};
                 
-//             }
-//             console.log(Data[1]);
+            }
+            console.log(Data[1]);
 
-//         res.json({result:Data});
-//     });
-//   });
-//   router.get('/subTable',function(req,res){
-//     // const sql = "Select *  from DATASIGMA.dbo.QitemBom where Code = @itemCode";
-//     const sql = "Select *  from DATASIGMA.dbo.QitemBom";
-//     const itemCode = req.query.itemCode;
-//     // console.log(itemCode)
-//     var db = new mssql.Request();
-//     // db.input('itemCode',mssql.VarChar(50),itemCode);
-//     db.query(sql,function(err,data,fields){
-//         try {
-//             let Data = data.recordset;
-//             let Data2 = data.recordsets[1];
-//             let NewData = new Array(Data.length);
-//             for(let i=0;i>1;i++){
-//                 console.log(Data[i])
-//                 // NewData[i] = Data2.map((e)=>{
-//                 //         console.log(e);
-//                 // })
+        res.json({result:Data});
+    });
+  });
+  router.get('/subTable',function(req,res){
+    // const sql = "Select *  from DATASIGMA.dbo.QitemBom where Code = @itemCode";
+    const sql = "Select *  from DATASIGMA.dbo.QitemBom";
+    const itemCode = req.query.itemCode;
+    // console.log(itemCode)
+    var db = new mssql.Request();
+    // db.input('itemCode',mssql.VarChar(50),itemCode);
+    db.query(sql,function(err,data,fields){
+        try {
+            let Data = data.recordset;
+            let Data2 = data.recordsets[1];
+            let NewData = new Array(Data.length);
+            for(let i=0;i>1;i++){
+                console.log(Data[i])
+                // NewData[i] = Data2.map((e)=>{
+                //         console.log(e);
+                // })
                 
-//             }
-//             // res.json({result:data});
+            }
+            // res.json({result:data});
 
-//                 // res.status(400).send({
-//                 //     result: "There was an issue signing up."
-//                 // });
-//         }
-//         catch(err) {
-//           console.log(err);
-//         }    
-//     });
-//   });
-//   router.post('/callback', (req, res) => {
-//     console.log(req.body)
-//     res.send("hi")
-//   });
-//   router.use((err,req,res,next)=>{
-//       const {status = 500} =err
-//       res.status(status).send('ERORR')
-//   })
+                // res.status(400).send({
+                //     result: "There was an issue signing up."
+                // });
+        }
+        catch(err) {
+          console.log(err);
+        }    
+    });
+  });
+  router.get('/callback', (req, res) => {
+    console.log(req.body)
+    res.send("hi")
+  });
+  router.post('/callback', (req, res) => {
+    console.log(req.body)
+    res.send("hi")
+  });
+  router.use((err,req,res,next)=>{
+      const {status = 500} =err
+      res.status(status).send('ERORR')
+  })
  
-//   module.exports = router;
+  module.exports = router;
