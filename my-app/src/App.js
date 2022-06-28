9ebdf5d9aa930fcad23a97a783a0e1858e4f8cd7
nutdanai.kan <bob6022800@gmail.com>
2022-06-28 11:12:23 +0700

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,7 +14,7 @@ function App() {
   const isDesktopOrLaptop = useMediaQuery({
     query: '(min-width: 1100px)'
   })
-  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 989px) and (min-width: 440px) ' })
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 989px) and (min-width: 400px) ' })
   localStorage.setItem('a', 'test');
  
   return (
